5ca82af729f9dfa82afcc1d5ef9d1cad84729ca9
lijiangt <lijiangt@gmail.com>
2018-03-30 10:48:18 +0800

    client register logic

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	metadata_impl "./metadata/impl"
+	register_cimpl "./register/client/impl"
+	register_pimpl "./register/provider/impl"
+
+	pbm "github.com/spolabs/nebula/tracker/metadata/pb"
+	pbrc "github.com/spolabs/nebula/tracker/register/client/pb"
+	pbrp "github.com/spolabs/nebula/tracker/register/provider/pb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 6666))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pbrp.RegisterProviderRegisterServiceServer(grpcServer, register_pimpl.NewProviderRegisterService())
+	pbrc.RegisterClientRegisterServiceServer(grpcServer, register_cimpl.NewClientRegisterService())
+	pbm.RegisterMatadataServiceServer(grpcServer, metadata_impl.NewMatadataService())
+	grpcServer.Serve(lis)
+}
